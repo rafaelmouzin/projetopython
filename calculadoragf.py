eb84fb5104f3a613dfb9afa42f151821c1f43a56
rafael mouzin <mouzinrafael@gmail.com>
2025-02-14 20:28:13 -0300

    calculadora

---
+++ calculadoragf.py
@@ -0,0 +1,35 @@
+nome = input("Digite seu nome: ")
+
+print(f"Olá, usuário {nome} Bem-vindo ao nosso sistema de gestão financeira.")
+print("Aqui, realizaremos diversos cálculos, como fluxo de caixa e outras operações financeiras para ajudar no seu controle.")
+
+
+print("""
+Digite o número referente à operação que deseja realizar:
+
+1 - Fluxo de caixa
+2 - Outra operação
+3 - Outra operação
+4 - Outra operação
+""")
+
+
+try:
+    num1 = int(input("Escolha uma opção: "))
+
+    if num1 == 1:
+        print("Você escolheu Fluxo de Caixa.")
+        
+    elif num1 == 2:
+        print(".")
+       
+    elif num1 == 3:
+        print(".")
+      
+    elif num1 == 4:
+        print(".")
+        
+    else:
+        print("Opção inválida. Tente novamente.")
+except ValueError:
+    print("Erro: Digite um número válido.")
